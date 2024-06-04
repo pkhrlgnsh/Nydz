c6c7fa6a476446cbfb7a862929ad56cdd3eeb424
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-04 15:50:27 +0545

    test

--- cft/contact.php
+++ cft/contact.php
@@ -1,49 +1,10 @@
 <?php
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Contact Us</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <div class="container">
-        <div class="header">
-            <img src="your-image-url.jpg" alt="Company Logo">
-            <h1>Contact Us</h1>
-        </div>
-        <div class="contact-section">
-            <div class="contact-form">
-                <h2>Get in Touch</h2>
-                <form action="/submit_form" method="post">
-                    <p>Your Name (required)<br>
-                        <input type="text" name="your-name" required>
-                    </p>
-                    <p>Your Email (required)<br>
-                        <input type="email" name="your-email" required>
-                    </p>
-                    <p>Subject<br>
-                        <input type="text" name="your-subject">
-                    </p>
-                    <p>Your Message<br>
-                        <textarea name="your-message" rows="5" required></textarea>
-                    </p>
-                    <p>
-                        <input type="submit" value="Send">
-                    </p>
-                </form>
-            </div>
-            <div class="contact-info">
-                <img src="contact-image.jpg" alt="Contact Image">
-            </div>
-        </div>
-        <div class="social-media">
-            <a href="https://www.facebook.com" target="_blank">F</a>
-            <a href="https://www.twitter.com" target="_blank">T</a>
-            <a href="https://www.instagram.com" target="_blank">I</a>
-            <a href="https://www.linkedin.com" target="_blank">L</a>
-        </div>
-    </div>
-</body>
-</html>
+/*
+Template Name: Contact Page
+*/
+get_header();
 ?>
+<div class="container">
+<?php echo do_shortcode('[contact-form-7 id="53f58ea" title="Contact form 1"]'); ?>
+</div>
+<?php get_footer();?>
--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -1,34 +1,62 @@
 <?php
-/**
-*
-* @package WordPress
-* @subpackage Twenty_Fourteen
-* @since Twenty Fourteen 1.0
+/*
+Template Name: HomePage
 */
 get_header() ?>
 <!-- Banner Section -->
-<section class="banner-section" style="text-align: center;">
-    <div class="container-fluid" style="background-image: url('http://localhost/jobsfungi/wp-content/uploads/2024/05/How-GoWithFund-Works-1.jpg'); background-size: fit; height: 80px;">
-        <div class="column">
-            <div class="col-12">
-                <h1 class="display-4" style="color: white; padding-top: 80px;">Welcome to Jobs in Europe</h1>
-            </div>
-        </div>
-    </div>
-</section>
-<section style="text-align: center;">
-    <div class="container-fluid" style="background: #FFFFF; height: 80px;">
-        <div class="column">
-            <div class="col-12">
-            <h6 class="display-4" style="color:blue;">"Welcome to Your European Career Portal"</h6>
-               
-            </div>
-        </div>
-    </div>
-</section>
+<style>
+    .banner_iomage_wraooper { 
+        background-image: url('http://localhost/jobsfungi/wp-content/uploads/2024/05/How-GoWithFund-Works-1.jpg'); 
+        background-size: fit; 
+        height: 30rem;
+        display: flex;
+        justify-content: center;
+    }
+    .banner_content_text{
+        display: flex;
+        align-items: flex-end;
+        justify-content: center;
+             
+    }
+    .why_us{
+        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+    }
+   .post_captions{
+    background-color: #00000099;
+}
+     .page_size{
+     width: auto;
+}
+    .blogs_post{
+        justify-content: center;
+    }
 
+    .banner_content_texts{
+      display: flex;
+      align-items: flex-end;
+      font-size: 3.5rem;
+      background-color: #00000099;
+      color: #ffff;
+      justify-content: center;
+      padding: 40px;
+      margin-bottom: 0px;
+    }
+    .banner_content_say{
+       color:white;
+    
+      }
+    </style>
+<section class="banner-section" style="text-align: center;">
+    <div class="container-fluid banner_iomage_wraooper">
+        <div class="column banner_content_text">
+            <div class="col-12 banner_content_texts">
+            <h1 class="display-4 banner_content_say">"Welcome to Your European Career Portal"</h1>
+            </div>
+        </div>
+    </div>
+</section>
     <div class="feature-posts">
-    <a href="single-post.html" class="feature-post-item">                       
+    <a href="single-post.html" class="feature-post-item ">                       
     <span style="font-size: 22px;">Featured Posts</span>
 </a>
 
@@ -48,7 +76,7 @@ get_header() ?>
         ?>
                 <a href="<?php echo esc_url(get_permalink()); ?>" class="feature-post-item">
                     <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" class="w-100" alt="">
-                    <div class="feature-post-caption"><?php the_title(); ?></div>
+                    <div class="feature-post-caption post_captions"><?php the_title(); ?></div>
                 </a>
         <?php
             endwhile;
@@ -62,7 +90,7 @@ get_header() ?>
 </section>
         <hr>
         <div class="page-container">
-            <div class="page-content">
+            <div class="page-content page_size">
                 
             <div class="card">
     <?php
@@ -89,14 +117,8 @@ get_header() ?>
             </div>
             <div class="card-body">
                 <div class="blog-media">
-                    <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" alt="" class="w-100">
-                    <?php
-                    $post_categories = get_the_category();
-                    if (!empty($post_categories)) {
-                        echo '<a href="' . esc_url(get_category_link($post_categories[0]->term_id)) . '" class="badge badge-primary">' . esc_html($post_categories[0]->name) . '</a>';
-                    }
-                    ?>
-                </div>
+                    <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" alt="" class="card_image_content">
+                                   </div>
                 <p class="my-3"><?php echo get_the_excerpt(); ?></p>
             </div>
             <div class="card-footer d-flex justify-content-between align-items-center flex-basis-0">
@@ -210,13 +232,24 @@ foreach ($tags as $tag) {
 
                 <div class="row">
     <div class="col-md-4">
-        <h5 class="sidebar-title mt-5 mb-4">Instagram</h5>
+        <h5 class="sidebar-title mt-5 mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue" class="bi bi-facebook" viewBox="0 0 20 20">
+  <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+</svg>    
+        Facebook</h5>
     </div>
     <div class="col-md-4">
-        <h5 class="sidebar-title mt-5 mb-4">Facebook</h5>
+        <h5 class="sidebar-title mt-5 mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#d62976" class="bi bi-instagram" viewBox="0 0 20 20">
+  <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+</svg>  
+        Instagram</h5>
     </div>
     <div class="col-md-4">
-        <h5 class="sidebar-title mt-5 mb-4">Twitter</h5>
+        <h5 class="sidebar-title mt-5 mb-4">
+        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#1DA1F2" class="bi bi-twitter-x" viewBox="0 0 20 20">
+  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
+</svg></br>Twitter</h5>
     </div>
 </div>
                
@@ -267,14 +300,14 @@ foreach ($tags as $tag) {
     </div>
     
     <section class="recent-blog-posts">
-    <div class="container">
+    <div class="ast-container">
         <h2 class="section-title text-center mb-4" style="color: black;">Recent Blog Posts</h2>
         <div class="row">
             <?php
             // WP_Query arguments to retrieve recent blog posts
             $blog_post_args = array(
                 'post_type' => 'post',
-                'posts_per_page' => 3 // Display the latest 3 blog posts
+                'posts_per_page' => 3 // Display the latest 4 blog posts
             );
 
             // The Query
@@ -294,7 +327,7 @@ foreach ($tags as $tag) {
                             <div class="blog-content">
                                 <h3 class="blog-title"><a href="<?php the_permalink(); ?>" style="color: black;"><?php the_title(); ?></a></h3>
                                 <p class="blog-excerpt" style="color: black;"><?php echo get_the_excerpt(); ?></p>
-                                <a href="<?php the_permalink(); ?>" class="read-more" style="color: black;">Read more</a>
+                                <a href="<?php the_permalink(); ?>" class="read-more" style="color: black; ">Read more</a>
                             </div>
                         </div>
                     </div>
@@ -310,9 +343,9 @@ foreach ($tags as $tag) {
     </div>
 </section>
 <section class="why-choose-us-section">
-    <div class="container">
+    <div class="container why_us">
         <h2 class="section-title text-center mb-4">Why Choose Us?</h2>
-        <div class="row">
+        <div class="row blogs_post">
             <div class="col-md-4">
                 <h3 class="reason-title">Quality Jobs</h3>
                 <p class="reason-description">We offer a wide range of high-quality job opportunities in Europe.</p>
