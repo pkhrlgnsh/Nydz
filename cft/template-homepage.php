ae753815457b4ae00b34ca651ed59734005dbdc1
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-05 15:04:43 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -191,7 +191,7 @@ if ($latest_post_query->have_posts()) :
                 </div>
                 <div class="card-body px-0">
                     <h3 class="card-title mb-2"><?php the_title(); ?></h3>
-                    <p class="my-2"><?php echo get_the_excerpt(); ?></p>
+                    <p class="my-2 text_post"><?php echo get_the_excerpt(); ?></p>
                 </div>
                 <div class="card-footer p-0 text-center">
                     <a href="<?php echo esc_url(get_permalink()); ?>" class="btn btn-outline-dark btn-sm button_hov">READ MORE</a>
@@ -343,7 +343,7 @@ foreach ($tags as $tag) {
                             </div>
                             <div class="blog-content">
                                 <h3 class="blog-title blog_titl-pd"><a href="<?php the_permalink(); ?>" style="color: black;"><?php the_title(); ?></a></h3>
-                                <p class="blog-excerpt blog_titl-pd" style="color: black;"><?php echo get_the_excerpt(); ?></p>
+                                <p class="blog-excerpt blog_titl-pd text_post" style="color: black;"><?php echo get_the_excerpt(); ?></p>
                                 <a href="<?php the_permalink(); ?>" class="read-more button_hov">Read more</a>
                             </div>
                         </div>
