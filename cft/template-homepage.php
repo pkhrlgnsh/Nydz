93f21ec038f2b1754aac7e5b7a58121daadaf723
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-05 12:27:48 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -202,7 +202,7 @@ endif;
 
             <!-- Sidebar -->
             <div class="page-sidebar text-center side_bar">
-                <h4 class="sidebar-title section-title mb-4 mt-3">About</h4>
+                <h3 class="sidebar-title section-title mb-4 mt-3">About</h3>
                 <img src="https://stagingjobseurope.fungiwonders.com/wp-content/uploads/2024/06/job-search-word-concepts-banner-vector-28884582.jpg" alt="" class="circle-100 mb-3">
                 <div class="socials mb-3 mt-2">
                     <a href="javascript:void(0)"><i class="ti-facebook"></i></a>
