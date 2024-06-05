5710bb0768220f2acb108d397f61429c081ba30c
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-05 13:55:58 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -339,7 +339,7 @@ foreach ($tags as $tag) {
                             </div>
                             <div class="blog-content">
                                 <h3 class="blog-title blog_titl-pd"><a href="<?php the_permalink(); ?>" style="color: black;"><?php the_title(); ?></a></h3>
-                                <p class="blog-excerpt" style="color: black;"><?php echo get_the_excerpt(); ?></p>
+                                <p class="blog-excerpt blog_titl-pd" style="color: black;"><?php echo get_the_excerpt(); ?></p>
                                 <a href="<?php the_permalink(); ?>" class="read-more button_hov">Read more</a>
                             </div>
                         </div>
