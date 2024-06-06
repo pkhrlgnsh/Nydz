48f480932350ce67f8ba3ce88e6a3d8b9417ef4c
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 15:17:16 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -209,12 +209,7 @@ if ($latest_post_query->have_posts()) :
                 <div class="card-header p-0">
                     <div class="blog-media blog_titl_pd">
                         <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" alt="" class="w-100">
-                        <?php
-                        $post_categories = get_the_category();
-                        if (!empty($post_categories)) {
-                            echo '<a href="' . esc_url(get_category_link($post_categories[0]->term_id)) . '" class="badge badge-primary blog_titl_pds">' . esc_html($post_categories[0]->name) . '</a>';
-                        }
-                        ?>
+                        <p class="badge badge-primary blog_titl_pds">latest</p>
                     </div>
                 </div>
                 <div class="card-body px-0">
