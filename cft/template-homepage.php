8388d66f979d3f8a101e3b979d7f1a797ba35708
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 09:24:33 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -31,6 +31,7 @@ get_header() ?>
     color: white;
     font-size: 16px;
     cursor: pointer;
+    margin: auto;
   }
     .blog_titl_pd{
         padding-top: 10px;
@@ -149,8 +150,7 @@ get_header() ?>
                 <p class="my-3 text_post"><?php echo get_the_excerpt(); ?></p>
             </div>
             <div class="card-footer d-flex justify-content-between align-items-center flex-basis-0">
-                <button class="btn btn-primary circle-35 mr-4"><i class="ti-back-center"></i></button>
-                <a href="<?php echo esc_url(get_permalink()); ?>" class="btn btn-outline-dark btn-sm button_hov">Read More</a>
+                             <a href="<?php echo esc_url(get_permalink()); ?>" class="btn btn-outline-dark btn-sm button_hov">Read More</a>
     </div>
     <?php
         endwhile;
