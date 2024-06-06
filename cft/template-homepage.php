87b640acf6f78f743de2a6ef77bcb57c3587269f
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 14:57:12 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -24,19 +24,22 @@ get_header() ?>
         font-size: 14px;
     }
 
+    .button_hovs
+    {
+    padding-block: 20px;
+    padding-bottom: 2px;
+    padding-left: 3px;
+    padding-right: 3px;
+    padding-top: 2px;
+    background-color: #33A2FF !important;
+    color: white;
+    font-size: 12px;
+    cursor: pointer;
+    }
     .button_hovs:hover
     {
     background-color: orange !important;
     color: white !important;
-    }
-    .button_hovs
-    {
-    padding: 8px; /* Adjusted padding */
-    background-color: #33A2FF !important;
-    color: white;
-    font-size: 14px;
-    cursor: pointer;
-    margin: auto;
     }
     .button_hov
     {
