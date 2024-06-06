34999bd8fbe1648fa5be62de544827141cfbf768
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 11:32:14 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -2,7 +2,8 @@
 /*
 Template Name: HomePage
 */
-get_header() ?>
+get_header();
+?>
 <!-- Banner Section -->
 <style>
     .banner_iomage_wraooper { 
@@ -16,16 +17,13 @@ get_header() ?>
         display: flex;
         align-items: flex-end;
         justify-content: center;
-    
     }
     .text_post{
         padding-top: 10px;
-        padding-bottom: px;
         font-size: 14px;
     }
-
     .button_hov {
-        padding: 8px; /* Adjusted padding */
+        padding: 8px;
         background-color: #33A2FF !important;
         color: white;
         font-size: 14px;
@@ -35,9 +33,7 @@ get_header() ?>
     .blog_titl_pd{
         padding-top: 10px;
         padding-bottom: 10px;
-    
     }
-
     .button_hov:hover {
         background-color: orange !important;
         color: white !important;
@@ -87,20 +83,17 @@ get_header() ?>
     </div>
 </section>
 <div class="feature-posts features_posts">
-    <a href="single-post.html" class="feature-post-item ">                       
+    <a href="single-post.html" class="feature-post-item">                       
         <span style="font-size: 22px;">Featured Posts</span>
     </a>
-
     <?php
     // WP_Query arguments
     $args = array(
         'category_name' => 'featured',
-        'posts_per_page' => 4 // Set to -1 to display all posts in the "featured" category
+        'posts_per_page' => 4
     );
-
     // The Query
     $featured_posts_query = new WP_Query($args);
-
     // The Loop
     if ($featured_posts_query->have_posts()) :
         while ($featured_posts_query->have_posts()) : $featured_posts_query->the_post();
@@ -111,29 +104,25 @@ get_header() ?>
             </a>
     <?php
         endwhile;
-        wp_reset_postdata(); // Reset Post Data
+        wp_reset_postdata();
     else :
-        // If no posts found
         echo 'No posts found in the "featured" category.';
     endif;
     ?>
 </div>
-</section>
 <hr>
 <div class="page-container">
-    <div class="page-content ">
+    <div class="page-content">
         <div class="card cards_top">
             <?php
             // WP_Query arguments to retrieve the latest post
             $latest_post_args = array(
-                'posts_per_page' => 1, // Retrieve only one post
+                'posts_per_page' => 1,
                 'orderby' => 'date',
-                'order' => 'DESC' // Get the latest post
+                'order' => 'DESC'
             );
-
             // The Query
             $latest_post_query = new WP_Query($latest_post_args);
-
             // The Loop
             if ($latest_post_query->have_posts()) :
                 while ($latest_post_query->have_posts()) : $latest_post_query->the_post();
@@ -152,9 +141,8 @@ get_header() ?>
                     </div>
             <?php
                 endwhile;
-                wp_reset_postdata(); // Reset Post Data
+                wp_reset_postdata();
             else :
-                // If no posts found
                 echo 'No posts found.';
             endif;
             ?>
@@ -164,15 +152,13 @@ get_header() ?>
             <?php
             // WP_Query arguments to retrieve the latest post
             $latest_post_args = array(
-                'posts_per_page' => 6, // Retrieve 6 posts
+                'posts_per_page' => 6,
                 'orderby' => 'date',
-                'order' => 'DESC', // Get the latest post
-                'offset' => 1 // Skip the first post
+                'order' => 'DESC',
+                'offset' => 1
             );
-
             // The Query
             $latest_post_query = new WP_Query($latest_post_args);
-
             // The Loop
             if ($latest_post_query->have_posts()) :
                 while ($latest_post_query->have_posts()) : $latest_post_query->the_post();
@@ -201,52 +187,46 @@ get_header() ?>
                     </div>
             <?php
                 endwhile;
-                wp_reset_postdata(); // Reset Post Data
+                wp_reset_postdata();
             else :
-                // If no posts found
                 echo 'No posts found.';
-            endif;
-            ?>
-        </div>
-        <button class="btn btn-primary btn-block my-4 button_hov" style="width: 200px; font-size: 16px; margin: auto;">Load More Posts</button>            
-    </div>
-
-    <!-- Sidebar -->
-    <div class="page-sidebar text-center side_bar">
-        <h3 class="sidebar-title section-title mb-4 mt-3">About</h3>
-        <img src="https://stagingjobseurope.fungiwonders.com/wp-content/uploads/2024/06/job-search-word-concepts-banner-vector-28884582.jpg" alt="" class="circle-100 mb-3">
-        <div class="socials mb-3 mt-2">
-            <a href="javascript:void(0)"><i class="ti-facebook"></i></a>
-            <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
-        </div>
-        <p>Welcome to Jobs in Europe, where your career dreams come to life. Discover a world of opportunities tailored to your skills and ambitions. Whether you are starting afresh or looking to advance your realm, we are here to support your journey in every step.</p>
-
-        <h4 class="sidebar-title mt-5 mb-4">Subscribe Us To Get Newsletter</h4>
-        <form action="">
-            <div class="subscribe-wrapper">
-                <input type="email" class="form-control" placeholder="Email Address">
-                <button type="submit" class="btn btn-primary"><i class="ti-location-arrow"></i></button>
-            </div>
-        </form>
-
-        <?php
-        // Array of tags
-        $tags = array("Light Driver Jobs", "Heavy Driver Jobs", "Agriculture Jobs", "Waiter & Waitress Jobs", "Care Giver Jobs");
-        ?>
-
-        <h4 class="sidebar-title mt-5 mb-4">Hot Tags</h4>
-        <div class="widget">
-            <ul class="list-unstyled list-unstyled-border tagcloud text-left">
-                <?php
-                foreach ($tags as $tag) {
-                    // Replace spaces with hyphens and convert to lowercase for the URL slug
-                    $tag_slug = strtolower(str_replace(' ', '-', $tag));
-                    // Generate the link
-                    echo '<li><a href="https://stagingjobseurope.fungiwonders.com/jobs/' . $tag_slug . '" class="badge badge-pill badge-light">' . $tag . '</a></li>';
-                }
                 ?>
-            </ul>
+                </div>
+                <button class="btn btn-primary btn-block my-4 button_hov" style="width: 200px; font-size: 16px; margin: auto;">Load More Posts</button>            
+            </div>
+            <!-- Sidebar -->
+            <div class="page-sidebar text-center side_bar">
+                <h3 class="sidebar-title section-title mb-4 mt-3">About</h3>
+                <img src="https://stagingjobseurope.fungiwonders.com/wp-content/uploads/2024/06/job-search-word-concepts-banner-vector-28884582.jpg" alt="" class="circle-100 mb-3">
+                <div class="socials mb-3 mt-2">
+                    <a href="javascript:void(0)"><i class="ti-facebook"></i></a>
+                    <a href="javascript:void(0)"><i class="ti-twitter"></i></a>
+                </div>
+                <p>Welcome to Jobs in Europe, where your career dreams come to life. Discover a world of opportunities tailored to your skills and ambitions. Whether you are starting afresh or looking to advance your realm, we are here to support your journey in every step.</p>
+                <h4 class="sidebar-title mt-5 mb-4">Subscribe Us To Get Newsletter</h4>
+                <form action="">
+                    <div class="subscribe-wrapper">
+                        <input type="email" class="form-control" placeholder="Email Address">
+                        <button type="submit" class="btn btn-primary"><i class="ti-location-arrow"></i></button>
+                    </div>
+                </form>
+                <?php
+                // Array of tags
+                $tags = array("Light Driver Jobs", "Heavy Driver Jobs", "Agriculture Jobs", "Waiter & Waitress Jobs", "Care Giver Jobs");
+                ?>
+                <h4 class="sidebar-title mt-5 mb-4">Hot Tags</h4>
+                <div class="widget">
+                    <ul class="list-unstyled list-unstyled-border tagcloud text-left">
+                        <?php
+                        foreach ($tags as $tag) {
+                            // Replace spaces with hyphens and convert to lowercase for link
+                            $tag_link = strtolower(str_replace(' ', '-', $tag));
+                            echo '<li><a href="' . get_site_url() . '/tag/' . $tag_link . '">' . $tag . '</a></li>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+            </div>
         </div>
-    </div>
-</div>
-<?php get_footer() ?>
+        <?php get_footer(); ?>
+       
