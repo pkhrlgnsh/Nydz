e5a81a8d3fdfb22528b77c7b6a3233674b8050c4
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 12:20:10 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -26,14 +26,14 @@ get_header() ?>
 
     .button_hovs
     {
-    padding: 10px;
+    padding: 15px;
     padding-bottom: 2px;
     padding-left: 3px;
     padding-right: 3px;
     padding-top: 2px;
     background-color: #33A2FF !important;
     color: white;
-    font-size: 12x;
+    font-size: 14x;
     cursor: pointer;
     }
     .button_hovs:hover
