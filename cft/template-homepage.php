cd8dcfb18ead2e4f2a85d007c5bea2dc03cf94ba
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 14:54:26 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -24,19 +24,7 @@ get_header() ?>
         font-size: 14px;
     }
 
-    .button_hovs
-    {
-    padding: 20px;
-    padding-bottom: 2px;
-    padding-left: 3px;
-    padding-right: 3px;
-    padding-top: 2px;
-    background-color: #33A2FF !important;
-    color: white;
-    font-size: 12px;
-    cursor: pointer;
-    }
-    .button_hovs:hover
+        .button_hovs:hover
     {
     background-color: orange !important;
     color: white !important;
