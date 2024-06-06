35dabba5c82bbaefb4f5d7b15e5df0615f29f882
pkhrlgnsh <pkhrlgnsh@gmail.com>
2024-06-06 14:52:12 +0545

    test

--- cft/template-homepage.php
+++ cft/template-homepage.php
@@ -55,7 +55,9 @@ get_header() ?>
         padding-bottom: 10px;
     
     }
-
+    .blog_titl_pds{
+        padding:5px;
+    }
   .button_hov:hover {
     background-color: orange !important;
     color: white !important;
@@ -201,7 +203,7 @@ if ($latest_post_query->have_posts()) :
         <div class="col-lg-6">
             <div class="card text-center mb-5">
                 <div class="card-header p-0">
-                    <div class="blog-media blog_titl_pd">
+                    <div class="blog-media blog_titl_pds">
                         <img src="<?php echo esc_url(get_the_post_thumbnail_url(null, 'large')); ?>" alt="" class="w-100">
                         <?php
                         $post_categories = get_the_category();
